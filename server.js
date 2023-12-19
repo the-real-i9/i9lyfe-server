b868f0b9131efa1927d5c3089603edd79dacfd74
i9 <oluwarinolasam@gmail.com>
2023-12-19 20:39:44 +0100

    added quickTest function

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 import app from "./app.js"
 
+const quickTest = async () => {
+  
+}
+
 app.listen(5000, () => {
   console.log(`Server running at http://localhost:${process.env.PORT || 5000}`)
+  quickTest()
 })
