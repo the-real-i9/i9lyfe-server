294fc0da0c6610eb6ae0d8050ee0c31869c7cbc7
i9 <oluwarinolasam@gmail.com>
2023-12-19 13:17:57 +0100

    db pool singleton

--- models/db.js
+++ models/db.js
@@ -1,6 +1,17 @@
-import { Pool } from "pg";
+import { Pool } from "pg"
 
-const pool = new Pool()
+class PoolSingleton {
+  constructor(poolConfig) {
+    if (!PoolSingleton.instance) {
+      PoolSingleton.instance = new Pool(poolConfig)
+      return PoolSingleton.instance
+    }
+
+    return PoolSingleton.instance
+  }
+}
+
+const pool = new PoolSingleton()
 
 export const query = (queryText, values, callback) => {
   return pool.query(queryText, values, callback)
