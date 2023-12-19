e63d40f4681fa9c729ea3a3032ff7262e8ff1d73
i9 <oluwarinolasam@gmail.com>
2023-12-19 20:28:16 +0100

    moved express session to app.js

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
 import express from "express"
-import authRoutes from "./routes/auth_routes.js"
+import expressSession from 'express-session'
+import pgSession from 'connect-pg-simple'
+
+import authRoutes from "./routes/authRoutes.js"
+import { getDBPool } from "./models/db.js"
+
+(await import('dotenv')).config()
 
 const app = express()
+const PGStore = pgSession(expressSession)
+
+app.use(
+  "/auth/signup",
+  expressSession({
+    store: new PGStore({
+      pool: getDBPool(),
+      tableName: "ongoing_registration",
+      createTableIfMissing: true,
+    }),
+    resave: false,
+    saveUninitialized: false,
+    // eslint-disable-next-line no-undef
+    secret: process.env.SIGNUP_SESSION_COOKIE_SECRET,
+    cookie: {
+      maxAge: 1 * 60 * 60 * 1000,
+      secure: false,
+    }
+  })
+)
 
 app.use("/auth", authRoutes)
 
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,4 @@
 import express from "express"
-import expressSession from "express-session"
-import pgSession from "connect-pg-simple"
-import { getDBPool } from "../models/db.js"
 
 import {
   emailVerificationController,
@@ -11,26 +8,6 @@ import {
 } from "../controllers/authControllers.js"
 
 const router = express.Router()
-
-const PGStore = pgSession(expressSession)
-router.use(
-  "/signup",
-  expressSession({
-    store: new PGStore({
-      pool: getDBPool(),
-      tableName: "ongoing_registration",
-      createTableIfMissing: true,
-    }),
-    resave: false,
-    saveUninitialized: false,
-    // eslint-disable-next-line no-undef
-    secret: process.env.SIGNUP_SESSION_COOKIE_SECRET,
-    cookie: {
-      maxAge: 1 * 60 * 60 * 1000,
-      secure: false,
-    }
-  })
-)
 
 router.post("/signup/request_new_account", newAccountRequestController)
 
