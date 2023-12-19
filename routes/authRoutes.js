5b82ad6e089f9d47762899437a12a55e72ef8eda
i9 <oluwarinolasam@gmail.com>
2023-12-19 13:19:48 +0100

    registration session setup

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,18 +1,42 @@
 import express from "express"
+import expressSession from "express-session"
+import pgSession from "connect-pg-simple"
+import { Pool } from "pg"
+
 import {
   emailVerificationController,
-  registrationRequestController,
+  newAccountRequestController,
   signinController,
   signupController,
 } from "../controllers/authControllers.js"
 
 const router = express.Router()
 
-router.post("/registration_request", registrationRequestController)
+const PGStore = pgSession(expressSession)
+router.use(
+  "/signup",
+  expressSession({
+    store: new PGStore({
+      pool: new Pool(),
+      tableName: "ongoing_registration",
+      createTableIfMissing: true,
+    }),
+    resave: false,
+    saveUninitialized: false,
+    // eslint-disable-next-line no-undef
+    secret: process.env.SIGNUP_SESSION_COOKIE_SECRET,
+    cookie: {
+      maxAge: 1 * 60 * 60 * 1000,
+      secure: false,
+    }
+  })
+)
 
-router.post("/email_verification", emailVerificationController)
+router.post("/signup/request_new_account", newAccountRequestController)
 
-router.post("/signup", signupController)
+router.post("/signup/verify_email", emailVerificationController)
+
+router.post("/signup/register_user", signupController)
 
 router.post("/signin", signinController)
 
