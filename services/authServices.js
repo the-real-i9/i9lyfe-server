56079d3a85ed29159ed9fe9c5a57a873caf16d2e
i9 <oluwarinolasam@gmail.com>
2023-12-19 13:23:07 +0100

    newAccoutRequestService pseudocode

--- services/authServices.js
+++ services/authServices.js
@@ -1,5 +1,12 @@
-const registrationRequestService = () => {}
+const newAccountRequestService = (email) => {
+  // check if a user with email exists, if yes return { ok: false, err: { code: 422, reason: 'A user with the email already exists' } }; else
+  // generate a verification code and send to the email
+  // create a potential_users session for the email, along with verification data
+  // return {ok: true, err: null}
+}
 
-const emailVerificationService = () => {}
+const emailVerificationService = (email) => {
 
-const mailingService = () => {}
+}
+
+
