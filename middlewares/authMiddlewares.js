a3609b080c60ae3640001985b945cdacf045045d
i9 <oluwarinolasam@gmail.com>
2023-12-20 17:45:40 +0100

    auth middlewares in routes

---
+++ middlewares/authMiddlewares.js
@@ -0,0 +1,42 @@
+/**
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ * @param {import('express').NextFunction} next
+ */
+export const confirmOngoingRegistration = (req, res, next) => {
+  if (!req.session.potential_user_verfification_data) {
+    return res.status(403).send({ errorMessage: "No ongoing registration!" })
+  }
+
+  next()
+}
+
+/**
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ * @param {import('express').NextFunction} next
+ */
+export const rejectVerifiedEmail = (req, res, next) => {
+  if (req.session.potential_user_verfification_data.verified) {
+    return res
+      .status(403)
+      .send({ errorMessage: "Your email has already being verified!" })
+  }
+
+  next()
+}
+
+/**
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ * @param {import('express').NextFunction} next
+ */
+export const rejectUnverifiedEmail = (req, res, next) => {
+  if (!req.session.potential_user_verfification_data.verified) {
+    return res
+      .status(403)
+      .send({ errorMessage: "Your email has not been verified!" })
+  }
+
+  next()
+}
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,14 +6,29 @@ import {
   signinController,
   signupController,
 } from "../controllers/authControllers.js"
+import {
+  confirmOngoingRegistration,
+  rejectUnverifiedEmail,
+  rejectVerifiedEmail,
+} from "../middlewares/authMiddlewares.js"
 
 const router = express.Router()
 
 router.post("/signup/request_new_account", newAccountRequestController)
 
-router.post("/signup/verify_email", emailVerificationController)
+router.post(
+  "/signup/verify_email",
+  confirmOngoingRegistration,
+  rejectVerifiedEmail,
+  emailVerificationController
+)
 
-router.post("/signup/register_user", signupController)
+router.post(
+  "/signup/register_user",
+  confirmOngoingRegistration,
+  rejectUnverifiedEmail,
+  signupController
+)
 
 router.post("/signin", signinController)
 
