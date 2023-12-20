5f5d5e0272614edc31915d78e0c0140ed1b718bd
i9 <oluwarinolasam@gmail.com>
2023-12-20 17:46:15 +0100

    request new account test

--- tests/authRoutes.test.js
+++ tests/authRoutes.test.js
@@ -5,9 +5,8 @@ import app from "../app.js"
 import { userAlreadyExists } from "../services/authServices.js"
 ;(await import("dotenv")).config()
 
-
 describe("POST /auth/signup/request_new_account", () => {
-  it("it should be successful (status 200), except user already exists (status 422)", async () => {
+  it("it should request new account or fail, if user with email already exists", async () => {
     try {
       const testEmail = "oluwarinolasam@gmail.com"
 
@@ -17,8 +16,13 @@ describe("POST /auth/signup/request_new_account", () => {
           email: testEmail,
         })
 
-      if (await userAlreadyExists(testEmail)) expect(res.statusCode).toBe(422)
-      else expect(res.statusCode).toBe(200)
+      if (await userAlreadyExists(testEmail)) {
+        expect(res.statusCode).toBe(422)
+        console.log(res.body.reason)
+      } else {
+        expect(res.statusCode).toBe(200)
+        console.log(res.body.msg)
+      }
     } catch (error) {
       expect(error).toMatch("error")
     }
