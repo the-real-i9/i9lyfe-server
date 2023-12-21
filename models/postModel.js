4ebc0ca0d9e52f72ecc4bb0abd1de26463d35227
i9 <oluwarinolasam@gmail.com>
2023-12-21 16:31:54 +0100

    postModel with some declarations

---
+++ models/postModel.js
@@ -0,0 +1,19 @@
+export const createNewPost = async () => { // let isRepost flag be passed if possible
+
+}
+
+export const createNewMention = async () => {
+
+}
+
+export const createNewHashtagAction = async () => {
+
+}
+
+export const createNewReaction = async () => {
+
+}
+
+export const createNewComment = async () => {
+
+}
