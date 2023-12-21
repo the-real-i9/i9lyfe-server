dcf371509b77d465b9173924ef8cc5cc0024a8b0
i9 <oluwarinolasam@gmail.com>
2023-12-21 10:12:53 +0100

    generateJwtToken helper

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,8 +1,18 @@
+import jwt from "jsonwebtoken"
+
 export const commaSeparateString = (str) => str.replaceAll(" ", ", ")
 
 export const generateCodeWithExpiration = () => {
   const code = Math.trunc(Math.random() * 900000 + 100000)
-  const expirationTime = new Date(Date.now() + (1 * 60 * 60 * 1000))
+  const expirationTime = new Date(Date.now() + 1 * 60 * 60 * 1000)
 
   return [code, expirationTime]
+}
+
+/**
+ * @param {string|Buffer|JSON} payload
+ * @returns {string} A JWT Token
+ */
+export const generateJwtToken = (payload) => {
+  return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1h" })
 }
