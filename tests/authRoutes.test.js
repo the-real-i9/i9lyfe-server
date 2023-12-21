34ceff83895dbc651af981088091a5995a60eaf5
i9 <oluwarinolasam@gmail.com>
2023-12-21 14:28:52 +0100

    testing improved | test for /auth/signin

--- tests/authRoutes.test.js
+++ tests/authRoutes.test.js
@@ -1,30 +1,35 @@
 import { describe, it, expect } from "@jest/globals"
 import request from "supertest"
 import app from "../app.js"
-
-import { userExists } from "../services/authServices.js"
 ;(await import("dotenv")).config()
 
 describe("POST /auth/signup/request_new_account", () => {
   it("it should request new account or fail, if user with email already exists", async () => {
-    try {
-      const testEmail = "oluwarinolasam@gmail.com"
+    const testEmail = "oluwarinolasa@gmail.com"
 
-      const res = await request(app)
-        .post("/auth/signup/request_new_account")
-        .send({
-          email: testEmail,
-        })
+    const res = await request(app)
+      .post("/auth/signup/request_new_account")
+      .send({
+        email: testEmail,
+      })
 
-      if (await userExists(testEmail)) {
-        expect(res.statusCode).toBe(422)
-        console.log(res.body.reason)
-      } else {
-        expect(res.statusCode).toBe(200)
-        console.log(res.body.msg)
-      }
-    } catch (error) {
-      expect(error).toMatch("error")
-    }
+    expect(res.statusCode).toBe(200)
+    expect(res.body).toHaveProperty("msg")
+  })
+})
+
+describe("POST /auth/signin", () => {
+  it("it should pass on correct credentials or fail otherwise", async () => {
+    // const [testEmail, testPassword] = ["samuel123@gmail.com", "sammyken"] // incorrect credentials
+    // const [testEmail, testPassword] = ["oluwarinolasam@gmail.com", "incfhunmytor"] // incorrect password credential
+    const [testEmail, testPassword] = ["oluwarinolasam@gmail.com", "fhunmytor"] // correct credentials
+
+    const res = await request(app).post("/auth/signin").send({
+      email: testEmail,
+      password: testPassword,
+    })
+
+    expect(res.statusCode).toBe(200)
+    expect(res.body).toHaveProperty("jwtToken")
   })
 })
