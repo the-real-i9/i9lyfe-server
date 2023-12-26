158f2df3113f2ad7dac1f4fdf9ff29de1aeeadd8
i9 <oluwarinolasam@gmail.com>
2023-12-26 20:17:43 +0100

    post creation route

--- routes/post_managementRoutes.js
+++ routes/post_managementRoutes.js
@@ -1,6 +1,8 @@
 import express from "express"
-import { postCreationController } from "../controllers/appControllers"
+import { postCreationController } from "../controllers/post_managementControllers.js"
 
 const router = express.Router()
 
 router.post("/create_post", postCreationController)
+
+export default router
