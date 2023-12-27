ea41bb1d7fc0eb2b766126a02d2156a0baa1a77e
i9 <oluwarinolasam@gmail.com>
2023-12-27 20:39:32 +0100

    react_to_post route

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,8 +1,10 @@
 import express from "express"
-import { postCreationController } from "../controllers/postControllers.js"
+import { postCreationController, postReactionController } from "../controllers/postControllers.js"
 
 const router = express.Router()
 
 router.post("/create_post", postCreationController)
+
+router.post("/react_to_post", postReactionController)
 
 export default router
