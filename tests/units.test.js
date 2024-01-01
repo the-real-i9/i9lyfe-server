5798f3e7a36ea7599c36536ac92fdf4d599cf099
i9 <oluwarinolasam@gmail.com>
2024-01-01 10:51:53 +0100

    unit tests update

--- tests/units.test.js
+++ tests/units.test.js
@@ -1,6 +1,6 @@
 import { test, expect } from "@jest/globals"
 import { extractHashtags, extractMentions } from "../utils/helpers.js"
-import { multipleRowsParameters } from "../utils/subDBtasks.js"
+import { updateUserProfile } from "../models/UserModel.js"
 
 test("extract mentions", () => {
   const text = "This is a text with @kenny you @samuel"
@@ -23,4 +23,14 @@ test("create multiple rows parameters", () => {
 
   expect(res(3, 2)).toBe("($1, $2), ($3, $4), ($5, $6)")
   expect(res(3, 3)).toBe("($1, $2, $3), ($4, $5, $6), ($7, $8, $9)")
+})
+
+test("generate user profile update string", async () => {
+  const res = await updateUserProfile(3, { name: "Kenny", username: "Bimbo" })
+
+  console.log(res)
+  expect(res.text).toBe('UPDATE "User" SET name = $1, username = $2 WHERE id = $3')
+  expect(res.values[0]).toBe("Kenny")
+  expect(res.values[1]).toBe("Bimbo")
+  expect(res.values[2]).toBe(3)
 })
