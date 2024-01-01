bb2519a9f019c47adc99082521068080f9333096
i9 <oluwarinolasam@gmail.com>
2024-01-01 17:33:31 +0100

    began listing GETs

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,5 +1,6 @@
 import express from "express"
 import dotenv from "dotenv"
+import { expressjwt } from "express-jwt"
 
 import {
   followUserController,
@@ -7,7 +8,6 @@ import {
   updateUserProfileController,
   uploadProfilePictureController,
 } from "../controllers/UserControllers.js"
-import { expressjwt } from "express-jwt"
 
 dotenv.config()
 
@@ -34,5 +34,34 @@ router.post("/unfollow_user", unfollowUserController)
 router.put("/update_user_profile", updateUserProfileController)
 
 router.put("/upload_profile_picture", uploadProfilePictureController)
+
+/* All gets: belonging to session user */
+// GET user profile data
+router.get("/me")
+
+// GET user followers
+router.get("/me/followers")
+router.get("/:username/followers")
+
+// GET user followings
+router.get("/me/followings")
+router.get("/:username/followings")
+
+// GET user posts
+router.get("/me/posts")
+
+// GET posts user has been mentioned in
+router.get("/me/mentions")
+router.get("/:username/mentions")
+
+// GET posts reacted by user
+router.get("/me/likes")
+router.get("/:user_id/mentions")
+
+// GET posts saved by this user
+router.get("/saved_posts")
+
+// GET user notifications
+router.get("/notifications")
 
 export default router
