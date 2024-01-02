ffc25e4c3351ecb8852ce8f7540c0c5d7aa9e5ae
i9 <oluwarinolasam@gmail.com>
2024-01-02 15:42:18 +0100

    ExploreDiscover route

---
+++ routes/public/ExploreDiscover.js
@@ -0,0 +1,21 @@
+import express from 'express'
+
+const router = express.Router()
+
+/* Explore/Discover  */
+// GET all explore/discover contents (all posts) aggregated algorithmically based on various stats. Basically, this is like the route Instagram makes call to to render its Explore/Discover
+
+// GET all posts with search text
+
+// GET photo posts with search text
+
+// GET video posts with search text
+
+// GET reel posts with search text
+
+// GET story posts with search text
+
+// GET hashtags with search text
+
+/* Hashtags */
+// GET all posts with an hashtag_name
