f0e77260fd3766bdb5586c2a801fcdc6c5527c7e
i9 <oluwarinolasam@gmail.com>
2024-01-05 20:26:57 +0100

    initialized web sockets with authorization

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
+import { createServer } from 'http'
+import jwt from 'jsonwebtoken'
+
 import app from "./app.js"
 
-const quickTest = async () => {
-  
-}
+const server = createServer(app)
 
-app.listen(5000, () => {
+import { Server } from 'socket.io'
+import { NotificationService } from './services/NotificationService.js'
+
+export const io = new Server(server)
+
+io.use((socket, next) => {
+  const token = socket.handshake.headers.authorization
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) return next(new Error(err.message));
+    socket.jwt_payload = decoded;
+    next();
+  });
+})
+
+io.on("connection", (socket) => {
+  NotificationService.initWebSocket(io, socket)
+})
+
+server.listen(5000, 'localhost', () => {
   console.log(`Server running at http://localhost:${process.env.PORT || 5000}`)
-  quickTest()
 })
