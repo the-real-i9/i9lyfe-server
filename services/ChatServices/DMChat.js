eac8ab72bf538d2ae875e0ad1c74e7d4b1b99af8
i9 <oluwarinolasam@gmail.com>
2024-01-17 15:43:52 +0100

    DMChat: sendMessage, getConversationHistory

--- services/ChatServices/DMChat.js
+++ services/ChatServices/DMChat.js
@@ -2,40 +2,56 @@ import * as ChatModel from "../../models/ChatModel.js"
 import { getDBClient } from "../../models/db.js"
 
 export class DMChat {
-  constructor(conversation_id) {
+  constructor(client_user_id, conversation_id) {
+    this.client_user_id = client_user_id
     this.conversation_id = conversation_id
   }
 
   async createDM(participantsUserIds) {
     const dbClient = await getDBClient()
     try {
-      await dbClient.query('BEGIN')
-      
-      const conversation_id = await ChatModel.createConversation({ type: "direct" })
-  
-      await ChatModel.createUserConversation({ participantsUserIds, conversation_id })
+      await dbClient.query("BEGIN")
 
-      dbClient.query('COMMIT')
+      const conversation_id = await ChatModel.createConversation(
+        { type: "direct" },
+        dbClient
+      )
+
+      await ChatModel.createUserConversation(
+        { participantsUserIds, conversation_id },
+        dbClient
+      )
+
+      dbClient.query("COMMIT")
 
       return conversation_id
     } catch (error) {
-      dbClient.query('ROLLBACK')
+      dbClient.query("ROLLBACK")
       throw error
     } finally {
       dbClient.release()
     }
-
   }
 
-  sendMessage() {
+  async sendMessage(msg_content) {
+    await ChatModel.createMessage({
+      sender_id: this.client_user_id,
+      conversation_id: this.conversation_id,
+      msg_content,
+    })
 
+    // Implement realtime todos where appropriate
   }
 
-  getMessages() {
-
+  async getConversationHistory(limit, offset) {
+    return await ChatModel.getConversationHistory({
+      conversation_id: this.conversation_id,
+      limit,
+      offset,
+    })
   }
 
-  reactToMessage() {
+  reactToMessage() {}
 
-  }
+  deleteMessage() {}
 }
