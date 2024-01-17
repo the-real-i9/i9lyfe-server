5c73a924c86ecf4cfb5ed4c08c4df5199af77663
i9 <oluwarinolasam@gmail.com>
2024-01-17 20:00:11 +0100

    GroupChat:: create group, add participants, join group, leave group

--- services/ChatServices/GroupChat.js
+++ services/ChatServices/GroupChat.js
@@ -2,10 +2,36 @@ import { getDBClient } from "../../models/db.js"
 import * as ChatModel from "../../models/ChatModel.js"
 
 export class GroupChat {
+  async #addParticipantsToGroup(
+    { client_username, participants, group_conversation_id },
+    dbClient
+  ) {
+    await ChatModel.createUserConversation(
+      {
+        participantsUserIds: participants.map(({ user_id }) => user_id),
+        conversation_id: group_conversation_id,
+      },
+      dbClient
+    )
+
+    await ChatModel.createGroupConversationActivityLog(
+      {
+        group_conversation_id,
+        activity_info: {
+          type: "participants_added",
+          added_by: client_username,
+          added_participants: participants.map(({ username }) => username),
+        },
+      },
+      dbClient
+    )
+  }
+
   /**
    * @param {object[]} participants
    * @param {number} participants.user_id
    * @param {string} participants.username
+   * @returns The data needed to display the group chat page history
    */
   async createGroup(client_username, participants) {
     const dbClient = await getDBClient()
@@ -19,26 +45,16 @@ export class GroupChat {
 
       // add participants to group
       // group membership will be crated by a TRIGGER, starting with the first user as "admin"
-      await ChatModel.createUserConversation(
-        {
-          participantsUserIds: participants.map(({ user_id }) => user_id),
-          conversation_id: group_conversation_id,
-        },
+      await this.#addParticipantsToGroup(
+        { client_username, participants, group_conversation_id },
         dbClient
       )
 
-      await ChatModel.createGroupConversationActivityLog({
-        group_conversation_id,
-        activity_info: {
-          type: "participants_added",
-          added_by: client_username,
-          added_participants: participants.map(({ username }) => username),
-        },
-      }, dbClient)
-
       dbClient.query("COMMIT")
 
-      return group_conversation_id
+      // Implement realtime todos where appropriate
+
+      return group_conversation_id // more
     } catch (error) {
       dbClient.query("ROLLBACK")
       throw error
@@ -47,15 +63,147 @@ export class GroupChat {
     }
   }
 
-  sendMessage() {}
+  async sendMessage({ sender_id, conversation_id, msg_content }) {
+    await ChatModel.createMessage({ sender_id, conversation_id, msg_content })
 
-  joinGroup() {}
+    // Implement realtime todos where appropriate
+  }
 
-  addParticipants() {}
+  async getConversationHistory({ conversation_id, limit, offset }) {
+    return await ChatModel.getConversationHistory({
+      conversation_id,
+      limit,
+      offset,
+    })
+  }
 
-  leaveGroup() {}
+  /**
+   * @param {object} participant
+   * @param {number} participant.user_id
+   * @param {string} participant.username
+   * @param {number} group_conversation_id
+   */
+  async joinGroup(participant, group_conversation_id) {
+    const dbClient = await getDBClient()
+    try {
+      await dbClient.query("BEGIN")
 
-  removeMember() {}
+      await ChatModel.createUserConversation(
+        {
+          participantsUserIds: [participant.user_id],
+          conversation_id: group_conversation_id,
+        },
+        dbClient
+      )
+
+      await ChatModel.createGroupConversationActivityLog(
+        {
+          group_conversation_id,
+          activity_info: {
+            type: "group_joined",
+            who_joined: participant.username,
+          },
+        },
+        dbClient
+      )
+
+      dbClient.query("COMMIT")
+
+      // Implement realtime todos where appropriate
+    } catch (error) {
+      dbClient.query("ROLLBACK")
+      throw error
+    } finally {
+      dbClient.release()
+    }
+  }
+
+  /**
+   * @param {object} param0
+   * @param {object} param0.client_user
+   * @param {number} param0.client_user.user_id
+   * @param {string} param0.client_user.username
+   * @param {object[]} param0.participants
+   * @param {number} param0.participants.user_id
+   * @param {string} param0.participants.username
+   * @param {number} param0.group_conversation_id
+   */
+  async addParticipants({ client_user, participants, group_conversation_id }) {
+    const dbClient = await getDBClient()
+    try {
+      await dbClient.query("BEGIN")
+
+      if (
+        !(await ChatModel.isGroupAdmin({
+          member_user_id: client_user.user_id,
+          group_conversation_id,
+        }, dbClient))
+      ) {
+        throw new Error("You must be a group admin to add participants!")
+      }
+
+      // add participants to group
+      // group membership will be crated by a TRIGGER, starting with the first user as "admin"
+      await this.#addParticipantsToGroup(
+        {
+          client_username: client_user.username,
+          participants,
+          group_conversation_id,
+        },
+        dbClient
+      )
+
+      dbClient.query("COMMIT")
+
+      // Implement realtime todos where appropriate
+    } catch (error) {
+      dbClient.query("ROLLBACK")
+      throw error
+    } finally {
+      dbClient.release()
+    }
+  }
+
+  /**
+   * @param {object} participant
+   * @param {number} participant.user_id
+   * @param {string} participant.username
+   * @param {number} group_conversation_id
+   */
+  async leaveGroup(participant, group_conversation_id) {
+    const dbClient = await getDBClient()
+    try {
+      await dbClient.query("BEGIN")
+
+      await ChatModel.updateUserConversation({
+        user_id: participant.user_id,
+        conversation_id: group_conversation_id,
+        updateKVPairs: new Map().set("deleted", true),
+      })
+
+      await ChatModel.createGroupConversationActivityLog(
+        {
+          group_conversation_id,
+          activity_info: {
+            type: "group_left",
+            who_left: participant.username,
+          },
+        },
+        dbClient
+      )
+
+      dbClient.query("COMMIT")
+
+      // Implement realtime todos where appropriate
+    } catch (error) {
+      dbClient.query("ROLLBACK")
+      throw error
+    } finally {
+      dbClient.release()
+    }
+  }
+
+  removeParticipant() {}
 
   changeGroupPhoto() {}
 
@@ -64,6 +212,4 @@ export class GroupChat {
   makeAdmin() {}
 
   removeFromAdmin() {}
-
-  getMessages() {}
 }
