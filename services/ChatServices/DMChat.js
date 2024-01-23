800535ebb54efabf7ba20397ac1afd7ae26d7109
i9 <oluwarinolasam@gmail.com>
2024-01-23 21:29:09 +0100

    createDM optimized with CTE

--- services/ChatServices/DMChat.js
+++ services/ChatServices/DMChat.js
@@ -1,5 +1,4 @@
 import * as ChatModel from "../../models/ChatModel.js"
-import { getDBClient } from "../../models/db.js"
 import { ChatRealtimeService } from "./ChatRealtimeService.js"
 
 export class DMChatService {
@@ -13,38 +12,13 @@ export class DMChatService {
    * @param {string} partner.username
    * @returns The data needed to display the DM chat page for the client
    */
-  async createDM(client, partner) {
-    const dbClient = await getDBClient()
-    try {
-      await dbClient.query("BEGIN")
-
-      const conversation_id = await ChatModel.createConversation(
-        { type: "direct", created_by: client.username },
-        dbClient
-      )
-
-      await ChatModel.createUserConversation(
-        {
-          participantsUserIds: [client.user_id, partner.user_id],
-          conversation_id,
-        },
-        dbClient
-      )
-
-      dbClient.query("COMMIT")
+  async createDMConversation(client, partner) {
+      const dm_conversation_id = await ChatModel.createDMConversation(client, partner.user_id)
 
       await ChatRealtimeService.createDMConversation({
         client_user_id: client.user_id,
         partner_user_id: partner.user_id,
-        conversation_id,
+        dm_conversation_id,
       })
-
-      return conversation_id // more
-    } catch (error) {
-      dbClient.query("ROLLBACK")
-      throw error
-    } finally {
-      dbClient.release()
-    }
   }
 }
