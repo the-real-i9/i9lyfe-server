2d17c31b5f59477815963f1200972eccebf48447
i9 <oluwarinolasam@gmail.com>
2024-05-01 15:47:37 +0100

    preparing chat.test.js

---
+++ tests/routes/protected/chat.test.js
@@ -0,0 +1,18 @@
+import { test, xtest, expect } from "@jest/globals"
+import axios from "axios"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const prefixPath = "http://localhost:5000/api/chat"
+const i9xJwt =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbGllbnRfdXNlcl9pZCI6MywiY2xpZW50X3VzZXJuYW1lIjoiaTl4IiwiaWF0IjoxNzEzOTA0OTUxfQ.f8DfuwetMyjWoipFQw54wkzIaMgrLCeRzTXKPFjQZdU"
+
+const dollypJwt =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbGllbnRfdXNlcl9pZCI6NCwiY2xpZW50X3VzZXJuYW1lIjoiZG9sbHlwIiwiaWF0IjoxNzEzOTA2MzgwfQ.h_vTw7aXvC3uuTpRExJFqdc8xRkOAfZeC0IbgoXM7nA"
+
+const axiosConfig = (authToken) => ({
+  headers: {
+    Authorization: `Bearer ${authToken}`,
+  },
+})
