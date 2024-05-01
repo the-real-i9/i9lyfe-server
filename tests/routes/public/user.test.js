3b4f04365d4b960ca152771ead1b2bf5756c9742
i9 <oluwarinolasam@gmail.com>
2024-05-01 15:10:23 +0100

    (testing) user routes: get user following

--- tests/routes/public/user.test.js
+++ tests/routes/public/user.test.js
@@ -13,11 +13,18 @@ xtest("get user profile", async () => {
   expect(res.data).toHaveProperty("profileData.user_id")
 })
 
-test("get user followers", async () => {
+xtest("get user followers", async () => {
   const res = await axios.get(prefixPath + "/dollyp/followers")
 
   expect(res.status).toBe(200)
   expect(res.data).toHaveProperty("userFollowers")
+})
+
+test("get user following", async () => {
+  const res = await axios.get(prefixPath + "/dollyp/following")
+
+  expect(res.status).toBe(200)
+  expect(res.data).toHaveProperty("userFollowing")
 
   console.log(res.data)
 })
