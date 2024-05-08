3e1e9465099576a9322b44f82950a2b794a7db8c
i9 <oluwarinolasam@gmail.com>
2024-05-08 15:55:55 +0100

    remove constructor function for UserService

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -4,7 +4,7 @@ export const getSessionUserController = async (req, res) => {
   try {
     const { client_user_id } = req.auth
 
-    const clientUser = await new UserService(client_user_id).getClientUser()
+    const clientUser = await UserService.getClientUser(client_user_id)
 
     res.status(200).send({ clientUser })
   } catch (error) {
@@ -19,11 +19,11 @@ export const followUserController = async (req, res) => {
 
     const { client_user_id } = req.auth
 
-    await new UserService(client_user_id).follow(to_follow_user_id)
+    await UserService.follow(client_user_id, to_follow_user_id)
 
     res.sendStatus(200)
   } catch (error) {
-    // console.error(error)
+    console.error(error)
     res.sendStatus(500)
   }
 }
@@ -34,7 +34,7 @@ export const unfollowUserController = async (req, res) => {
 
     const { client_user_id } = req.auth
 
-    await new UserService(client_user_id).unfollow(followee_user_id)
+    await UserService.unfollow(client_user_id, followee_user_id)
 
     res.sendStatus(200)
   } catch (error) {
@@ -49,7 +49,8 @@ export const editUserController = async (req, res) => {
 
     const { client_user_id } = req.auth
 
-    const updatedUserData = await new UserService(client_user_id).updateProfile(
+    await UserService.updateProfile(
+      client_user_id,
       Object.entries(updateDict)
     )
 
@@ -66,9 +67,7 @@ export const updateUserConnectionStatusController = async (req, res) => {
 
     const { client_user_id } = req.auth
 
-    await new UserService(client_user_id).updateConnectionStatus(
-      new_connection_status
-    )
+    await UserService.updateConnectionStatus(client_user_id, new_connection_status)
 
     res.sendStatus(200)
   } catch (error) {
@@ -83,7 +82,7 @@ export const readUserNotificationController = async (req, res) => {
 
     const { client_user_id } = req.auth
 
-    await new UserService(client_user_id).readNotification(notification_id)
+    await UserService.readNotification(notification_id, client_user_id)
 
     res.sendStatus(200)
   } catch (error) {
@@ -107,9 +106,7 @@ export const getUserProfileController = async (req, res) => {
   try {
     const { username } = req.params
 
-    const profileData = await new UserService(
-      req.auth?.client_user_id
-    ).getProfile(username)
+    const profileData = await UserService.getProfile(username, req.auth?.client_user_id)
 
     res.status(200).send({ profileData })
   } catch (error) {
@@ -122,9 +119,7 @@ export const getUserFollowersController = async (req, res) => {
   try {
     const { username } = req.params
 
-    const userFollowers = await new UserService(
-      req.auth?.client_user_id
-    ).getFollowers(username)
+    const userFollowers = await UserService.getFollowers(username, req.auth?.client_user_id)
 
     res.status(200).send({ userFollowers })
   } catch (error) {
@@ -137,9 +132,7 @@ export const getUserFollowingController = async (req, res) => {
   try {
     const { username } = req.params
 
-    const userFollowing = await new UserService(
-      req.auth?.client_user_id
-    ).getFollowing(username)
+    const userFollowing = await UserService.getFollowing(username, req.auth?.client_user_id)
 
     res.status(200).send({ userFollowing })
   } catch (error) {
@@ -152,8 +145,9 @@ export const getUserPostsController = async (req, res) => {
   try {
     const { username } = req.params
 
-    const userPosts = await new UserService(req.auth?.client_user_id).getPosts(
-      username
+    const userPosts = await UserService.getPosts(
+      username,
+      req.auth?.client_user_id
     )
 
     res.status(200).send({ userPosts })
@@ -166,9 +160,7 @@ export const getUserMentionedPostsController = async (req, res) => {
   try {
     const { client_user_id } = req.auth
 
-    const mentionedPosts = await new UserService(
-      client_user_id
-    ).getMentionedPosts()
+    const mentionedPosts = await UserService.getMentionedPosts(client_user_id)
 
     res.status(200).send({ mentionedPosts })
   } catch (error) {
@@ -181,7 +173,7 @@ export const getUserReactedPostsController = async (req, res) => {
   try {
     const { client_user_id } = req.auth
 
-    const reactedPosts = await new UserService(client_user_id).getReactedPosts()
+    const reactedPosts = await UserService.getReactedPosts(client_user_id)
 
     res.status(200).send({ reactedPosts })
   } catch (error) {
@@ -194,7 +186,7 @@ export const getUserSavedPostsController = async (req, res) => {
   try {
     const { client_user_id } = req.auth
 
-    const savedPosts = await new UserService(client_user_id).getSavedPosts()
+    const savedPosts = await UserService.getSavedPosts(client_user_id)
 
     res.status(200).send({ savedPosts })
   } catch (error) {
@@ -209,9 +201,7 @@ export const getUserNotificationsController = async (req, res) => {
 
     const { client_user_id } = req.auth
 
-    const notifications = await new UserService(
-      client_user_id
-    ).getNotifications(from)
+    const notifications = await UserService.getNotifications(client_user_id, from)
 
     res.status(200).send({ notifications })
   } catch (error) {
--- services/user.service.js
+++ services/user.service.js
@@ -2,73 +2,69 @@ import * as UM from "../models/user.model.js"
 import { NotificationService } from "./notification.service.js"
 
 export class UserService {
-  constructor(client_user_id) {
-    this.client_user_id = client_user_id
+  static async getClientUser(client_user_id) {
+    return await UM.getUser(client_user_id)
   }
 
-  async getClientUser() {
-    return await UM.getUser(this.client_user_id)
-  }
-
-  async follow(to_follow_user_id) {
-    const followNotifData = await UM.followUser(this.client_user_id, to_follow_user_id)
+  static async follow(client_user_id, to_follow_user_id) {
+    const followNotifData = await UM.followUser(client_user_id, to_follow_user_id)
 
     const { receiver_user_id, ...restData } = followNotifData
     new NotificationService(receiver_user_id).pushNotification(restData)
   }
 
-  async updateProfile(updateKVPairs) {
-    await UM.editUser(this.client_user_id, updateKVPairs)
+  static async updateProfile(client_user_id, updateKVPairs) {
+    await UM.editUser(client_user_id, updateKVPairs)
   }
 
-  async updateConnectionStatus(new_connection_status) {
-    await UM.updateUserConnectionStatus(this.client_user_id, new_connection_status)
+  static async updateConnectionStatus(client_user_id, new_connection_status) {
+    await UM.updateUserConnectionStatus(client_user_id, new_connection_status)
   }
 
-  async readNotification(notification_id) {
-    await UM.readUserNotification(notification_id, this.client_user_id)
+  static async readNotification(notification_id, client_user_id) {
+    await UM.readUserNotification(notification_id, client_user_id)
   }
 
-  async uploadProfilePicture() {
+  static async uploadProfilePicture() {
     // upload binary to CDN and get back file URL
   }
 
   /* GETs */
-  async getProfile(username) {
-    return await UM.getUserProfile(username, this.client_user_id)
+  static async getProfile(username, client_user_id) {
+    return await UM.getUserProfile(username, client_user_id)
   }
 
-  async getFollowers(username) {
-    return await UM.getUserFollowers(username, this.client_user_id)
+  static async getFollowers(username, client_user_id) {
+    return await UM.getUserFollowers(username, client_user_id)
   }
 
-  async getFollowing(username) {
-    return await UM.getUserFollowing(username, this.client_user_id)
+  static async getFollowing(username, client_user_id) {
+    return await UM.getUserFollowing(username, client_user_id)
   }
 
-  async getPosts(username) {
-    return await UM.getUserPosts(username, this.client_user_id)
+  static async getPosts(username, client_user_id) {
+    return await UM.getUserPosts(username, client_user_id)
   }
 
-  async getMentionedPosts() {
-    return await UM.getMentionedPosts(this.client_user_id)
+  static async getMentionedPosts(client_user_id) {
+    return await UM.getMentionedPosts(client_user_id)
   }
 
-  async getReactedPosts() {
-    return await UM.getReactedPosts(this.client_user_id)
+  static async getReactedPosts(client_user_id) {
+    return await UM.getReactedPosts(client_user_id)
   }
 
-  async getSavedPosts() {
-    return await UM.getSavedPosts(this.client_user_id)
+  static async getSavedPosts(client_user_id) {
+    return await UM.getSavedPosts(client_user_id)
   }
 
   /** @param {Date} from  */
-  async getNotifications(from) {
-    return await UM.getUnreadNotifications(this.client_user_id, from)
+  static async getNotifications(client_user_id, from) {
+    return await UM.getUnreadNotifications(client_user_id, from)
   }
 
   /* DELETEs */
-  async unfollow(followee_user_id) {
-    await UM.unfollowUser(this.client_user_id, followee_user_id)
+  static async unfollow(client_user_id, followee_user_id) {
+    await UM.unfollowUser(client_user_id, followee_user_id)
   }
 }
