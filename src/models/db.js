2df14c9b1ce66e30d01cd75a62ee8eb572762462
i9 <oluwarinolasam@gmail.com>
2024-07-08 19:23:07 +0100

    connection string change

--- src/models/db.js
+++ src/models/db.js
@@ -3,10 +3,7 @@ import pg from "pg"
 const { Pool } = pg
 
 const pool = new Pool({
-  connectionString:
-    process.env.NODE_ENV === "production"
-      ? `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:5432/${process.env.POSTGRES_DB}`
-      : process.env.DABABASE_URI,
+  connectionString: process.env.DATABASE_URI,
 })
 
 /** @param {import('pg').QueryConfig} query */
