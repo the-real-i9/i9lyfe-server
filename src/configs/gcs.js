8b08e1603ee705785998f591bc5eb40866400e1c
i9 <oluwarinolasam@gmail.com>
2024-11-07 23:29:37 +0100

    file upload working

--- src/configs/gcs.js
+++ src/configs/gcs.js
@@ -3,9 +3,7 @@ import { Storage } from "@google-cloud/storage"
 export const storageBucketName = "i9lyfe-bucket"
 
 const bucket = new Storage({
-  credentials: {
-    apiKey: process.env.GCS_API_KEY,
-  }
+  apiKey: process.env.GCS_API_KEY
 }).bucket(storageBucketName)
 
 export const getStorageBucket = () => {
